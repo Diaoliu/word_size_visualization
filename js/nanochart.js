bf9af9efaca48fcde363bcb6d5954bbc999aa38d
Diao Liu <diaoliu@gmail.com>
2017-07-20 19:28:26 +0200

    add user interaction

--- js/nanochart.js
+++ js/nanochart.js
@@ -9,7 +9,6 @@ Nanochart.prototype.addData = function(file, callback) {
     var self = this;
     $.ajax({
         url: file,
-        async: false,
         timeout: 3000,
         error: function() {
             alert('Can not load CSV file!');
@@ -21,19 +20,51 @@ Nanochart.prototype.addData = function(file, callback) {
                 },
                 complete: function(results) {
                     self._parseData(results.data);
+                    callback(self);
                 }
             });
         }
     });
-    return this;
 };
 
 Nanochart.prototype.addSparkline = function(query, series, charttype) {
-    return this._addChart(query, series, charttype, 'sparkline');
+    var self = this,
+        node = $('#' + query),
+        data, options, chart;
+    data = this._getData(series);
+    if (data) {
+        node.addClass('nc-sparkline');
+        options = this._sparklineOptions(charttype);
+        if (charttype === 'bar')
+            chart = new Chartist.Bar(node[0], data, options);
+        else if (charttype === 'line')
+            chart = new Chartist.Line(node[0], data, options);
+        chart.on('created', function(context) {
+                self._addSparklineLabel(chart, charttype);
+        });
+        this.charts.push(chart);
+    }
+    return this;
 };
 
 Nanochart.prototype.addFigure = function(query, series, charttype) {
-    return this._addChart(query, series, charttype, 'figure');
+    var self = this,
+        node = $('#' + query),
+        data, options, chart;
+    data = this._getData(series);
+    if (data) {
+        node.addClass('nc-figure');
+        options = this._figureOptions(charttype);
+        if (charttype === 'bar')
+            chart = new Chartist.Bar(node[0], data, options);
+        else if (charttype === 'line')
+            chart = new Chartist.Line(node[0], data, options);
+        chart.on('created', function(context) {
+                self._addFigureLabel(chart, charttype);
+        });
+        this.charts.push(chart);
+    }
+    return this;
 };
 
 Nanochart.prototype.addLink = function(query, targets, series, filter) {
@@ -42,8 +73,12 @@ Nanochart.prototype.addLink = function(query, targets, series, filter) {
         data = self.database.series[series];
     if (data && node) {
         node.addClass('nc-text');
-        if (filter)
-            data = filter(self.database.series[series]);
+        if (filter) {
+            data = data.map(function(element, index, array) {
+                if (filter(element, index, array)) 
+                    return element;
+            });
+        }
         node.hover(function () {
             targets.forEach(function(target) {
                 self.addSeries(target, data);
@@ -74,43 +109,26 @@ Nanochart.prototype.replaceSeries = function(chartName, series) {
     this._update('replace', chartName, series); 
 };
 
-Nanochart.prototype._addChart = function(query, series, charttype, type) {
-    var self = this;
-    var node = $('#' + query);
-    if (self.database.series 
-        && self.database.series.hasOwnProperty(series)) {
-        var data = {
-            labels: self.database.labels,
-            series: [self.database.series[series]]
+Nanochart.prototype._getData = function(series) {
+    if (this.database.series 
+        && this.database.series.hasOwnProperty(series)) {
+        return {
+            labels: this.database.labels,
+            series: [this.database.series[series]]
         };
-        var options;
-        if (type === 'sparkline'){
-            node.addClass('nc-sparkline');
-            options = this._sparklineOptions(charttype);
-        }
-        else if (type === 'figure') {
-            node.addClass('nc-figure');
-            options = this._figureOptions(charttype);
-        }
-
-        if (charttype === 'bar')
-            this.charts.push(Chartist.Bar(node[0], data, options));
-        else if (charttype === 'line')
-            this.charts.push(new Chartist.Line(node[0], data, options));
+    } else {
+        return undefined;
     }
-    return this;
-}
+};
 
 Nanochart.prototype._sparklineOptions = function(charttype) {
     var options = {
         axisX: {
             showLabel: false,
-            showGrid: false,
             offset: 0
         },
         axisY: {
             showLabel: false,
-            showGrid: false,
             offset: 0
         },
         high: this.database.max,
@@ -132,7 +150,9 @@ Nanochart.prototype._sparklineOptions = function(charttype) {
             lineSmooth: false,
             chartPadding: {
                 top: 5,
+                right: 0,
                 bottom: 5,
+                left: 0
             },
             fullWidth: true
         });
@@ -144,21 +164,101 @@ Nanochart.prototype._figureOptions = function(charttype) {
     var options = {
         high: this.database.max,
         low: this.database.min,
+        axisX: { labelOffset: {
+              x: 0,
+              y: 5
+            } },
+        showGridBackground: true,
         seriesBarDistance: 0
     };
 
     if (charttype === 'line') {
         Object.assign(options, {
             lineSmooth: false,
+            axisX: { labelOffset: {
+              x: -15,
+              y: 5
+            } },
             fullWidth: true,
-            showArea: true,
-            chartPadding: {
-                right: 50
-            }
+            showArea: true
         });
     }
     return options;
 };
+
+Nanochart.prototype._addSparklineLabel = function(chart, charttype) {
+    var self      = this,
+        sparkline = $(chart.container),
+        tooltip   = sparkline.find('.nc-sparkline-label'),
+        line      = sparkline.find('.ct-grids .ct-horizontal').first(),
+        label     = chart.data.labels[0] || 0,
+        data      = chart.data.series[0][0] || 0,
+        svg       = $(chart.svg.getNode());
+
+    if (tooltip.length == 0)
+        tooltip = $('<span class="nc-sparkline-label"></span>');
+
+    tooltip.html(label + '<br>' + data);
+    sparkline.append(tooltip);
+
+    svg.mousemove(function(event) {
+        var x = event.clientX - svg.offset().left,
+            cellWidth = parseInt(svg.attr('width')) / chart.data.labels.length,
+            index = Math.floor( x / cellWidth ),
+            label = chart.data.labels[index] || 0,
+            data  = chart.data.series[0][index] || 0;
+
+        if (charttype === 'line')
+            cellWidth = parseInt(svg.attr('width'))  / (self.database.labels.length - 1);
+
+        line.attr('x1', x).attr('x2', x)
+            .attr('y1', 0).attr('y2', svg.attr('height')).show();
+        tooltip.html(label + '<br>' + data);
+    });
+}
+
+Nanochart.prototype._addFigureLabel = function(chart, charttype) {
+    var self    = this,
+        figure  = $(chart.container),
+        tooltip = figure.find('.nc-tooltip'),
+        grid    = figure.find('.ct-grid-background'),
+        left    = parseInt(grid.attr('x')),
+        top     = parseInt(grid.attr('y')),
+        right   = parseInt(grid.attr('width')) + left,
+        bottom  = parseInt(grid.attr('height')) + top,
+        svg     = $(chart.svg.getNode());
+
+    if (tooltip.length == 0)
+        tooltip = $('<div class="nc-tooltip"></div>').hide();
+
+    figure.append(tooltip);
+
+    svg.mousemove(function(event) {
+        var x = event.clientX - svg.offset().left,
+            y = event.clientY - svg.offset().top,
+            cellWidth = ( right - left ) / (self.database.labels.length - 1);
+            
+        if (charttype === 'bar')
+            cellWidth = ( right - left ) / self.database.labels.length;
+
+        var index = Math.floor( ( x - left ) / cellWidth ),
+            line  = figure.find('.ct-grids .ct-horizontal').first(),
+            label = chart.data.labels[index],
+            data  = chart.data.series[0][index] || 0;
+
+        if (x >= left && x <= right
+            && y > top && y < bottom) {
+            tooltip.html(label + ': ' + data + ' ' + self.database.unit);
+            tooltip.show();
+            line.attr('x1', x).attr('x2', x).attr('y1', top).show();
+        } else {
+            line.hide();
+            tooltip.hide();
+        }
+        
+        tooltip.css('left', x - tooltip.outerWidth() / 2 + 'px');
+    });
+}
 
 Nanochart.prototype._update = function(action, chartName, series) {
     this.charts.forEach(function(chart) {
@@ -179,9 +279,10 @@ Nanochart.prototype._update = function(action, chartName, series) {
 Nanochart.prototype._parseData = function(data) {
     var database  = {}, 
         all       = [],
-        firstLine = data.shift();
+        firstLine = data.shift(),
+        unit      = firstLine.shift();
     /* convert data format */
-    firstLine.shift();
+    database.unit   = unit;
     database.labels = firstLine;
     database.series = {};
     data.forEach(function(line) {
@@ -199,13 +300,19 @@ Nanochart.prototype._parseData = function(data) {
     this.database = database;
 };
 
+/* new entity instance */
 var nano = new Nanochart();
-nano.addData('/csv/data.csv')
-    .addSparkline('papers-pro-year', 'papers pro year', 'bar')
-    .addFigure('papers', 'papers pro year', 'bar')
-    .addLink('first-approaches', ['papers-pro-year', 'papers'], 'papers pro year', function(series) {
-        return series.map(function(value, index) {
-            return (index <= 7)? value : 0;
-        });
-    })
-    .addLink('technique', ['papers'], 'technique papers');
+/* before adding sparkline or figure,
+ * it needs to add data provider first */
+nano.addData('/csv/data.csv', function(self) {
+    /* parameters: mount node, series name, chart type */
+    self.addSparkline('papers-pro-year', 'papers pro year', 'bar')
+        .addFigure('papers', 'papers pro year', 'bar')
+        /* parameters: mount node, target sparklines or figures,
+         * series name, and user defined data filter function    */
+        .addLink('first-approaches', ['papers-pro-year', 'papers'], 
+            'papers pro year', function(element, index, array) {
+                return index < 7;
+        })
+        .addLink('technique', ['papers'], 'technique papers');
+});
